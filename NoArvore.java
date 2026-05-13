ef26eed03c5c1ee6528e441acb2f5c1b6af536ec
Thiago-Brandao <m.oliveira@estudantes.ifg.edu.br>
2026-05-13 19:43:34 -0300

    Initial commit

---
+++ NoArvore.java
@@ -0,0 +1,11 @@
+public class NoArvore {
+    int valor;
+    NoArvore esquerda;
+    NoArvore direita;
+
+    public NoArvore (int valor) {
+        this.valor = valor;
+        esquerda = null;
+        direita = null;
+    }
+}
